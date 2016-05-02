74f2b1861815f70851e86b4e6cb60d6db82b471a
Marito <mariobrassescogarcia@gmail.com>
2016-05-02 16:45:36 +0200

    Add tournament, player and registration model and migration. Create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Player.create(name: "Mike")
+Player.create(name: "Pol")
+Player.create(name: "Paul")
+Player.create(name: "Tim")
+Player.create(name: "Sam")
+
+Tournament.create(name: "Fifa")
+Tournament.create(name: "Pro")
+Tournament.create(name: "Tekken")
+
+Registration.create(player_id: 1, tournament_id: 1)
+Registration.create(player_id: 1, tournament_id: 2)
+Registration.create(player_id: 1, tournament_id: 3)
+Registration.create(player_id: 2, tournament_id: 1)
+Registration.create(player_id: 2, tournament_id: 2)
+Registration.create(player_id: 2, tournament_id: 3)
+Registration.create(player_id: 3, tournament_id: 1)
+Registration.create(player_id: 3, tournament_id: 2)
+Registration.create(player_id: 4, tournament_id: 2)
+Registration.create(player_id: 4, tournament_id: 3)
